1a05ffbece9856b22f8694544355918288529dc7
marlontri <marlontrifunovic@gmail.com>
2023-07-29 10:44:42 -0700

    Add tiling and rendering ability

--- hex_domain.py
+++ hex_domain.py
@@ -44,13 +44,49 @@ class HexDomain:
         """Fill the domain with lozenges to create the 'empty cube' tiling."""
         self.gridpoints = (1 + self.hexant() // 2) * self.in_hex()
 
-    def hex_adjacent(self):
-        ...
-    def can_add(self, pos):
-        """Returns True iff there is a hexagon of three lozenges centered at POS."""
-        arr = np.indices(self.gridpoints.shape)[1:self.size-1, 1:self.size-1]
-        rot1 = self.gridpoints[arr[0]-1, arr[1]-1, 1] ==
+    def rot_hex(self, x: int, y: int):
+        arr = [
+            (x, y, 0),
+            (x - 1, y, 1),
+            (x - 1, y, 0),
+            (x - 1, y - 1, 1),
+            (x, y - 1, 0),
+            (x, y - 1, 1),
+        ]
+        for i in range(3):
+            tmp = self.gridpoints[arr[i]]
+            self.gridpoints[arr[i]] = self.gridpoints[arr[i + 3]]
+            self.gridpoints[arr[i + 3]] = tmp
 
+    def can_add(self):
+        """Returns True iff there is a hexagon of three lozenges centered at POS."""
+        arr = np.indices(self.gridpoints.shape[:2])[:, 1:, 1:]
+        tri0 = self.gridpoints[arr[0], arr[1], 0]
+        tri1 = self.gridpoints[arr[0] - 1, arr[1], 1]
+        tri2 = self.gridpoints[arr[0] - 1, arr[1], 0]
+        tri3 = self.gridpoints[arr[0] - 1, arr[1] - 1, 1]
+        tri4 = self.gridpoints[arr[0], arr[1] - 1, 0]
+        tri5 = self.gridpoints[arr[0], arr[1] - 1, 1]
+
+        eq1 = (
+            (tri0 == tri1)
+            * (tri1 != tri2)
+            * (tri2 == tri3)
+            * (tri3 != tri4)
+            * (tri4 == tri5)
+            * (tri5 != tri0)
+        )
+        eq2 = (
+            (tri1 == tri2)
+            * (tri2 != tri3)
+            * (tri3 == tri4)
+            * (tri4 != tri5)
+            * (tri5 == tri0)
+            * (tri0 != tri1)
+        )
+
+        ret = np.array(np.where(eq1 + eq2)) + 1
+        return ret.transpose()
 
     def sample(self, iters=10, q=1):
         """Runs MCMC.
@@ -58,7 +94,6 @@ class HexDomain:
         iters -- number of steps to run for.
         q -- amount of bias to add to the coin flip.  q=1 is 50/50.
         """
-
 
     def draw(self, s_size=1000):
         """Displays this domain and all of its lozenges."""
@@ -76,28 +111,41 @@ class HexDomain:
             shape[0] += shape[1] / 2
             shape = shape * grid_pix
             color = color_palette[0] if style == 0 else color_map(style / max_style)
-            print(style, color)
+            if style == 0:
+                continue
             plt.fill(*shape, c=color)
         plt.axis("equal")
         plt.axis("off")
         plt.show()
 
-    def save(self, name):
+    def save(self, name = "default"):
         """Creates a npy file containing the lozenges of this domain.  This is
         helpful if you are sampling a large domain and want your computer to
         take a break.  Be careful, there's no error handling.
         """
-        np.save(f"{name}_points.npy", self.lozenge_points)
-        np.save(f"{name}_colors.npy", self.lozenge_colors)
+        np.save(f"{name}_points.npy", self.gridpoints)
 
-    def load(self, name):
+    def load(self, name = "default"):
         """Loads a previously saved set of lozenges.  Be careful, there's no
         error handling.
         """
-        self.lozenge_points = np.load(f"{name}_points.npy")
-        self.lozenge_colors = np.load(f"{name}_colors.npy")
+        self.gridpoints = np.load(f"{name}_points.npy")
 
 
-domain = HexDomain(3)
+domain = HexDomain(40)
 domain.fill_min()
+domain.load()
+
+for i in range(10000000):
+    if i % 200000 == 0:
+        print("Drawing started!")
+        domain.save()
+        domain.draw()
+    can_adds = domain.can_add() 
+    pick = tuple( random.choice(list(can_adds)))
+    #print(pick)
+    domain.rot_hex(*pick)
+
+
+
 domain.draw()
